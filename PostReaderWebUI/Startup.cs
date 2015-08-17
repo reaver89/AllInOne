c30d1fc8dc60f5fc3ba0172191182e73834cc5b2
vmakhnevich <beatbox89@mail.ru>
2015-08-17 22:38:29 +0300

    Rename project

---
+++ PostReaderWebUI/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+using PostReaderWebUI;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace PostReaderWebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
